777dc84248c9fa4fe4b59b6366defa29a9297e42
Pjotr Prins <pjotr.prins@thebird.nl>
2013-11-05 15:42:49 +0100

    update_varscan2 works

--- scripts/update_varscan2.sh
+++ scripts/update_varscan2.sh
@@ -11,21 +11,21 @@ while read ref tumor ; do
   echo "tumor=$tumor ref=$ref"
   for x in $tumor $ref ; do 
     name="${x%.*}_full_kinome_CoDeCZ_chr17"
-    echo Samtools mpileup of $name...
-    echo "~/opt/bin/samtools mpileup -B -f /data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta ../$name.bam > $name.mpileup"|~/izip/git/opensource/ruby/once-only/bin/once-only -v -d varscan2
+    echo ==== Create samtools mpileup of $x $name...
+    echo "~/opt/bin/samtools mpileup -B -f /data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta ../$name.bam > $name.mpileup"|~/izip/git/opensource/ruby/once-only/bin/once-only $* --pfff -v -d varscan2
   done
-  ref="${ref%.*}_full_kinome_CoDeCZ_chr17"
-  tumor="${tumor%.*}_full_kinome_CoDeCZ_chr17"
-  echo "Running varscan2..."
-  echo "java -jar ~/opt/lib/VarScan.v2.3.6.jar somatic $ref.mpileup $tumor.mpileup $ref-varscan --min-coverage 20 --somatic-p-value 0.001" | ~/izip/git/opensource/ruby/once-only/bin/once-only -d varscan2 -v 
+  ref2="${ref%.*}_full_kinome_CoDeCZ_chr17"
+  tumor2="${tumor%.*}_full_kinome_CoDeCZ_chr17"
+  echo "==== Running varscan2 on $ref"
+  echo "java -jar ~/opt/lib/VarScan.v2.3.6.jar somatic $ref2.mpileup $tumor2.mpileup $ref2-varscan --min-coverage 20 --somatic-p-value 0.001" | ~/izip/git/opensource/ruby/once-only/bin/once-only $* --pfff -d varscan2 -v 
 
-  @@ Check tumor & ref:
-  echo "Readcount..."
-  echo "~/opt/bin/bam-readcount  -f /data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta  ../$tumor.bam 17 > $tumor.readcount"| ~/izip/git/opensource/ruby/once-only/bin/once-only -d varscan 2 -v
-  echo "Running fpfilter..."
-  echo "perl $HOME/opt/bin/fpfilter.pl $ref-varscan.snp $tumor.readcount"|~/izip/git/opensource/ruby/once-only/bin/once-only -d varscan2 -v 
+  echo "==== Index with Samtools $tumor2.bam ..."
+  echo "samtools index $tumor2.bam"| ~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d . -v
+  echo "==== Readcount on tumor $tumor2..."
+  echo "~/opt/bin/bam-readcount -w 5 -f /data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta  ../$tumor2.bam 17 > $tumor2.readcount"| ~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d varscan2 -v
+  echo "Running fpfilter using ref $ref2..."
+  echo "perl $HOME/opt/bin/fpfilter.pl --output-basename $tumor2 $ref2-varscan.snp $tumor2.readcount"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d varscan2 -v $*
 
-  exit 1
 done < somatic_bams.txt
 
 
