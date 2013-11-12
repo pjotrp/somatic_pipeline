c41c5cbf80b31b6f2c83710c2ae6a7229a66b168
Pjotr Prins <pjotr.prins@thebird.nl>
2013-11-12 11:11:54 +0100

    Somatic sniper filters

--- scripts/update_somaticsniper.sh
+++ scripts/update_somaticsniper.sh
@@ -1,22 +1,22 @@
 #! /bin/sh
 
-tumor=../merged_MBC023T_F3_20130828_chr17_full_kinome_CoDeCZ_chr17.bam
-ref=../merged_MBC023R_F3_20130828_chr17_full_kinome_CoDeCZ_chr17.bam
-# tumor=../merged_MBC008T_F3_20130528_chr17_full_kinome_CoDeCZ_chr17.bam
-# ref=../merged_MBC008R_F3_20130528_chr17_full_kinome_CoDeCZ_chr17.bam
+mkdir -p somaticsniper
+
+tumor=merged_MBC023T_F3_20130828_chr17_full_kinome_CoDeCZ_chr17.bam
+ref=merged_MBC023R_F3_20130828_chr17_full_kinome_CoDeCZ_chr17.bam
+# tumor=merged_MBC008T_F3_20130528_chr17_full_kinome_CoDeCZ_chr17.bam
+# ref=merged_MBC008R_F3_20130528_chr17_full_kinome_CoDeCZ_chr17.bam
 echo ---- Tumor $tumor
-echo ---- Reference $tumor
-name=${tumor#*/}
-java6 -Xmx2g -Djava.io.tmpdir=$HOME/tmp -jar ~/opt/mutect/muTect-1.1.4.jar \
-  --analysis_type MuTect \
-  --reference_sequence /data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta \
-  --input_file:normal $ref \
-  --input_file:tumor $tumor \
-  --out $name.call_stats.txt \
-  --coverage_file $name.coverage.wig.txt 
+echo ---- Reference $ref
+  name="${tumor%.*}"
 
-# --dbsnp ~/Broad/dbsnp_132_b37.leftAligned.vcf \
-# --intervals 17:78000000-79000000 \
-# --cosmic hg19_cosmic_v54_120711.vcf
+  echo "bam-somaticsniper -f /data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta ../$tumor ../$ref $name.snp"| ~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d somaticsniper -v
 
+  echo "==== Index with Samtools $tumor ..."
+  echo "samtools index $tumor"| ~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d . -v
+
+  echo "==== Readcount on tumor $tumor..."
+  echo "~/opt/bin/bam-readcount -b 15 -w 5 -f /data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta  ../$tumor 17 > $tumor.readcount"| ~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d somaticsniper -v
+  echo "Running fpfilter using ref $ref..."
+  echo "perl $HOME/opt/somatic-sniper/src/scripts/fpfilter.pl --output-basename $tumor $name.snp --readcount-file $tumor.readcount"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d somaticsniper -v $*
 
