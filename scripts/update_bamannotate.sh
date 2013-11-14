3d161926936eb4d5d762929c54bea378ef6cb37d
Pjotr Prins <pjotr.prins@thebird.nl>
2013-11-14 12:12:14 +0100

    scripts

--- scripts/update_bamannotate.sh
+++ scripts/update_bamannotate.sh
@@ -19,6 +19,6 @@ while read ref tumor ; do
     echo "*** $tumorout ***"
     echo "*** $refout ***"
     echo "$bamannotate -out $tumorout -snv $refname*.snvs.snv1 -snv $tumorname*.snvs.snv1 -ref /data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta -bam $ref -bam $tumor"|  ~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d $dir -v --ignore-lock
-    echo "$inheritance -d -in $tumorout.bamAnn -out $tumorout -uaf_ids 3 -aff_ids 4 -min_cov 4"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d $dir -v --ignore-lock
+    echo "$inheritance -d -in $tumorout.bamAnn -out $tumorout -uaf_ids 3 -aff_ids 4 -min_cov 8"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d $dir -v --ignore-lock
 done < bamfiles_2.txt
 
--- scripts/update_varscan2.sh
+++ scripts/update_varscan2.sh
@@ -1,4 +1,17 @@
 #! /bin/bash
+#
+# Usage
+#
+#   update_varscan2.sh listfile
+#
+# where listfile contains a list of tab delimited ref-tumor samples
+#
+
+listfn=$1
+if [ -z $listfn ] ; then 
+  echo "Provide a tab delimited file with bamfile names" 
+  exit 1
+fi
 
 BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
 echo $BASEDIR
@@ -11,22 +24,22 @@ while read ref tumor ; do
   echo "tumor=$tumor ref=$ref"
   for x in $tumor $ref ; do 
     name="${x%.*}_full_kinome_CoDeCZ_chr17"
-    echo ==== Create samtools mpileup of $x $name...
-    echo "~/opt/bin/samtools mpileup -B -f /data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta ../$name.bam > $name.mpileup"|~/izip/git/opensource/ruby/once-only/bin/once-only $* --pfff -v -d varscan2
+    echo "==== Create samtools mpileup of $x (name $name)"
+    echo "~/opt/bin/samtools mpileup -B -f /data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta ../$x > $name.mpileup"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -v -d varscan2
   done
   ref2="${ref%.*}_full_kinome_CoDeCZ_chr17"
   tumor2="${tumor%.*}_full_kinome_CoDeCZ_chr17"
-  echo "==== Running varscan2 on $ref"
-  echo "java -jar ~/opt/lib/VarScan.v2.3.6.jar somatic $ref2.mpileup $tumor2.mpileup $ref2-varscan --min-coverage 20 --somatic-p-value 0.001" | ~/izip/git/opensource/ruby/once-only/bin/once-only $* --pfff -d varscan2 -v 
+  echo "==== Running varscan2 on $ref (ref $ref2)"
+  echo "java -jar ~/opt/lib/VarScan.v2.3.6.jar somatic $ref2.mpileup $tumor2.mpileup $ref2-varscan --min-coverage 10 --somatic-p-value 0.001" | ~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d varscan2 -v --debug
 
   echo "==== Index with Samtools $tumor2.bam ..."
   echo "samtools index $tumor2.bam"| ~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d . -v
   echo "==== Readcount on tumor $tumor2..."
   echo "~/opt/bin/bam-readcount -w 5 -f /data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta  ../$tumor2.bam 17 > $tumor2.readcount"| ~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d varscan2 -v
   echo "Running fpfilter using ref $ref2..."
-  echo "perl $HOME/opt/bin/fpfilter.pl --output-basename $tumor2 $ref2-varscan.snp $tumor2.readcount"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d varscan2 -v $*
+  echo "perl $HOME/opt/bin/fpfilter.pl --output-basename $tumor2 $ref2-varscan.snp $tumor2.readcount"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d varscan2 -v 
 
-done < somatic_bams.txt
+done < $listfn
 
 
 
