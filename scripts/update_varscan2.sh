a83910e06ed3282acc07b7535fbb6712aaf867fc
Pjotr Prins <pjotr.prins@thebird.nl>
2013-12-10 11:45:33 +0100

    varscan2: back to the split mpileup. Also using the processSomatic scoring

--- scripts/update_varscan2.sh
+++ scripts/update_varscan2.sh
@@ -16,30 +16,27 @@ fi
 BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
 echo $BASEDIR
 
-onceonly="~/izip/git/opensource/ruby/once-only/bin/once-only"
+onceonly="$HOME/izip/git/opensource/ruby/once-only/bin/once-only"
 refgenome=/data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta
+bed="$HOME/full_kinome_CoDeCZ_chr17.bed"
 
 mkdir -p varscan2
 
 IFS=$'\t'
-while read ref tumor ; do
-  echo "tumor=$tumor ref=$ref"
-  # for x in $tumor $ref ; do 
-  #   name="${x%.*}_full_kinome_CoDeCZ_chr17"
-  #   echo "==== Create samtools mpileup of $x (name $name)"
-  #   echo "~/opt/bin/samtools mpileup -B -q 10 -f /data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta ../$x > $name.mpileup"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -v -d varscan2
-  # done
+while read normal tumor ; do
+  echo "tumor=$tumor normal=$normal"
+  ref=$normal
+  for x in $tumor $ref ; do 
+    name="${x%.*}_full_kinome_CoDeCZ_chr17"
+    echo "==== Create samtools mpileup of $x (name $name)"
+    # check -E option
+    echo "~/opt/bin/samtools mpileup -B -q 10 -f $refgenome -l $bed ../$x > $x.mpileup"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -v -d varscan2
+  done
 
-  # Check also -E option
-  echo "~/opt/bin/samtools mpileup -f $refgenome -q 10 -B ../$normal ../$tumor > $normal-$tumor.mpileup"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -v -d varscan2
-  echo "java -jar ~/opt/lib/VarScan.v2.3.6.jar somatic $normal-$tumor.mpileup normal-tumor.varScan.output --mpileup 1 --min-coverage-normal 5 --min-coverage-tumor 8 --somatic-p-value 0.001 --strand-filter --min-var-freq 0.20"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -v -d varscan2
+  # --mpileup 1 option
+  echo "java -jar ~/opt/lib/VarScan.v2.3.6.jar somatic $normal.mpileup $tumor.mpileup $normal-$tumor.varScan.output --min-coverage-normal 5 --min-coverage-tumor 8 --somatic-p-value 0.001 --strand-filter --min-var-freq 0.20"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -v -d varscan2
 
-  ref2="${ref%.*}_full_kinome_CoDeCZ_chr17"
-  tumor2="${tumor%.*}_full_kinome_CoDeCZ_chr17"
-  # echo "==== Running varscan2 on $ref (ref $ref2)"
-  # echo "java -jar ~/opt/lib/VarScan.v2.3.6.jar somatic $ref2.mpileup $tumor2.mpileup $ref2-varscan --min-coverage-normal 5 --min-coverage-tumor 8 --somatic-p-value 0.001 --strand-filter --min-var-freq 0.20" | ~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d varscan2 -v --debug
-
-  java -jar VarScan.jar processSomatic [output.snp]
+  java -jar ~/opt/lib/VarScan.v2.3.6.jar processSomatic $normal-$tumor.varScan.output.snp
 
   echo "==== Index with Samtools $tumor2.bam ..."
   echo "samtools index $tumor2.bam"| ~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d . -v
@@ -47,6 +44,8 @@ while read ref tumor ; do
   echo "~/opt/bin/bam-readcount -w 5 -f /data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta  ../$tumor2.bam 17 > $tumor2.readcount"| ~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d varscan2 -v
   echo "Running fpfilter using ref $ref2..."
   echo "perl $HOME/opt/bin/fpfilter.pl --output-basename $tumor2 $ref2-varscan.snp $tumor2.readcount"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d varscan2 -v 
+
+ exit 
 
 done < $listfn
 
