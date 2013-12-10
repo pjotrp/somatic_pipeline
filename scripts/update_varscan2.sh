e86de5c9fb231055739a2ffd888e7fbe3333fbba
Pjotr Prins <pjotr.prins@thebird.nl>
2013-12-10 15:17:48 +0100

    Skip output files

--- scripts/update_varscan2.sh
+++ scripts/update_varscan2.sh
@@ -32,20 +32,20 @@ while read normal tumor ; do
     name="${x%.*}_full_kinome_CoDeCZ_chr17"
     echo "==== Create samtools mpileup of $x (name $name)"
     # check -E option
-    echo "~/opt/bin/samtools mpileup -B -q 10 -f $refgenome -l $bed ../$x > $x.mpileup"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -v -d varscan2
+    echo "~/opt/bin/samtools mpileup -B -q 10 -f $refgenome -l $bed ../$x > $x.mpileup"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -v -d varscan2 --skip $x.mpileup
   done
 
   # --mpileup 1 option
   echo "java -jar ~/opt/lib/VarScan.v2.3.6.jar somatic $normal.mpileup $tumor.mpileup $normal-$tumor.varScan.output --min-coverage-normal 5 --min-coverage-tumor 8 --somatic-p-value 0.001 --strand-filter --min-var-freq 0.20"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -v -d varscan2
 
-  echo "java -jar ~/opt/lib/VarScan.v2.3.6.jar processSomatic $normal-$tumor.varScan.output.snp"|~/izip/git/opensource/ruby/once-only/bin/once-only -v -d varscan2
+  echo "java -jar ~/opt/lib/VarScan.v2.3.6.jar processSomatic $normal-$tumor.varScan.output.snp"|~/izip/git/opensource/ruby/once-only/bin/once-only -v -d varscan2 --skip $normal-$tumor.varScan.output.snp
 
   # The following runs the alternative readcount tools (older?)
   #
   echo "==== Readcount on tumor $tumor (chr17)..."
-  echo "~/opt/bin/bam-readcount -w 5 -f $refgenome  ../$tumor 17 > $tumor.readcount"| ~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d varscan2 -v
+  echo "~/opt/bin/bam-readcount -w 5 -f $refgenome  ../$tumor 17 > $tumor.readcount"| ~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d varscan2 -v --skip $tumor.readcount
   echo "Running fpfilter using ref $ref..."
-  echo "perl $HOME/opt/bin/fpfilter.pl --output-basename $tumor $normal-$tumor.varScan.output.snp $tumor.readcount"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d varscan2 -v 
+  echo "perl $HOME/opt/bin/fpfilter.pl --output-basename $tumor $normal-$tumor.varScan.output.snp $tumor.readcount"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d varscan2 -v
 
 done < $listfn
 
