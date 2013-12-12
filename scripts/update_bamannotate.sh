1b6f48dc4cad9caf3c010381517eef38b36c1cbe
Pjotr Prins <pjotr.prins@thebird.nl>
2013-12-12 14:21:19 +0100

    Bamannotate runner

--- scripts/update_bamannotate.sh
+++ scripts/update_bamannotate.sh
@@ -1,10 +1,23 @@
 #! /bin/sh
 
+listfn=$1
+if [ -z $listfn ] ; then 
+  echo "Provide a tab delimited file with bamfile names" 
+  exit 1
+fi
+
 bamannotate=/data/common_scripts/stef/getBamAnnotate.pl
 inheritance=/data/common_scripts/stef/getInheritance.pl
 
 dir=bamannotate
 mkdir -p $dir
+
+BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
+echo $BASEDIR
+
+onceonly="$HOME/izip/git/opensource/ruby/once-only/bin/once-only"
+refgenome=/data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta
+bed="$HOME/full_kinome_CoDeCZ_chr17.bed"
 
 IFS=$'\t'
 while read ref tumor ; do
@@ -16,9 +29,16 @@ while read ref tumor ; do
   tumorout=${tumorname##*/}
   refout=${refname##*/}
 
-    echo "*** $tumorout ***"
-    echo "*** $refout ***"
-    echo "$bamannotate -out $tumorout -snv $refname*.snvs.snv1 -snv $tumorname*.snvs.snv1 -ref /data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta -bam $ref -bam $tumor"|  ~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d $dir -v --ignore-lock
-    echo "$inheritance -d -in $tumorout.bamAnn -out $tumorout -uaf_ids 3 -aff_ids 4 -min_cov 8"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d $dir -v --ignore-lock
-done < bamfiles_2.txt
+  tumorbam=`find /data/mapping/cancer -name $tumor`
+  tumorbase="${tumorbam%.*}"
+  refbam=`find /data/mapping/cancer -name $ref`
+  refbase="${refbam%.*}"
+  echo "*** $tumorbam ***"
+  echo "*** $refbam ***"
+  echo "*** $tumorout ***"
+  echo "*** $refout ***"
+  echo "$bamannotate -out $tumorout -snv $refbase*.snvs.snv1 -snv $tumorbase*.snvs.snv1 -ref /data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta -bam $refbam -bam $tumorbam"|  ~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d $dir -v 
+  echo "$inheritance -d -in $tumorout.bamAnn -out $tumorout -uaf_ids 3 -aff_ids 4 -min_cov 8"|~/izip/git/opensource/ruby/once-only/bin/once-only --pfff -d $dir -v 
+  exit
+done < $listfn
 
