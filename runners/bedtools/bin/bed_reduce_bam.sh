6b1e4cc4a508c383175a94795ce64d96f2586b6e
Pjotr Prins <pjotr.public01@thebird.nl>
2014-03-03 11:15:43 +0100

    Removing unused files

--- runners/bedtools/bin/bed_reduce_bam.sh
+++
@@ -1,9 +0,0 @@
-# Reduce BAM file by applying bed
-#
-# bed_reduce_bam.sh design.bed bamfile 
-
-design=$1
-bam=$2
-base1=`basename $design`
-
-~/opt/bedtools/bin/intersectBed -abam $bam -b $design > ${bam%.*}_${base1%.*}.bam
--- runners/varscan2/bin/varscan2.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-#
-#   varscan2.sh ref.fa design.bed tumor.bam ref.bam
-
-
