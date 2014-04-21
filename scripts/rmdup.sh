21e8a523975d050d3a3ff374eb011f633f166c55
Pjotr Prins <pjotr.prins@thebird.nl>
2014-04-21 16:36:50 +0200

    rmdup

--- scripts/rmdup.sh
+++ scripts/rmdup.sh
@@ -3,11 +3,18 @@
 # Simple rmdup loop
 #
 #   ./rmdup.sh < bamlist
+#
+# Example
+#
+#   ls -1 --color=never *.bam | rmdup.sh
+
+sambamba=$HOME/opt/bin/sambamba
+onceonly=$HOME/izip/git/opensource/ruby/once-only/bin/once-only
 
 while read bam ; do
-  echo ==== Remove duplicates $bam...
+  echo ==== $sambamba remove duplicates $bam...
   outfn=$(basename $bam .bam)_rmdup.bam
-  echo "$sambamba markdup -r $bam $outfn"| $onceonly --pfff -d . -v --out $outfn
+  echo "$sambamba markdup -r $bam $outfn"| $onceonly --pfff -d . -v -in $bam --out $outfn
   [ $? -ne 0 ] && exit 1
 done
 
