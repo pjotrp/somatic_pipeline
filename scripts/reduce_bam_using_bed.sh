90cc978058c7379bb7eda02e33550d3264f1ecea
Pjotr Prins <pjotr.prins@thebird.nl>
2014-04-21 09:18:57 +0200

    Directly use bedtools

--- scripts/reduce_bam_using_bed.sh
+++ scripts/reduce_bam_using_bed.sh
@@ -9,17 +9,24 @@
 # Create list with something like
 #
 #   find /data/mapping/cancer -name '*MBC*'|grep bam$
+#
+# e.g.
+#
+#   wgs01:~/data/run5/bam_reduced$ ~/opt/somatic_pipeline/scripts/reduce_bam_using_bed.sh ~/kinome_design_SS_V2_110811_nochr_annot_sorted.bed < /home/cog/pprins/current_mbc_bamlist.txt 
+#
 
 BEDTOOLS=$HOME/opt/bedtools
 BEDFILE=$1
+design="$(basename $BEDFILE .bed)"
 
 BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
 echo $BASEDIR
 
 while read bam ; do
   echo Reducing $bam...
-  echo "$BEDTOOLS/bin/bed_reduce_bam.sh $BEDFILE $bam"|~/izip/git/opensource/ruby/once-only/bin/once-only -v --ignore-lock --ignore-queue -d .
-  done
+
+  echo "$BEDTOOLS/bin/intersectBed -abam $bam -b $BEDFILE > $(basename $bam .bam).$design.bam"|~/izip/git/opensource/ruby/once-only/bin/once-only -v --ignore-lock --ignore-queue --pfff -d .
+  exit 0
 done
 
 
