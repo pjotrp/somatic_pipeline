71cd177cfef19f4f3f2d9cdeef62f70c685f9d62
Pjotr Prins <pjotr.prins@thebird.nl>
2014-04-22 16:08:27 +0200

    indxe

--- scripts/sambamba_index.sh
+++ scripts/sambamba_index.sh
@@ -13,7 +13,7 @@ onceonly=$HOME/izip/git/opensource/ruby/once-only/bin/once-only
 
 while read bam ; do
   echo ==== $sambamba index $bam...
-  outfn=$(basename $bam .bam).bai
+  outfn=$(basename $bam .bam).bam.bai
   echo "$sambamba index $bam"| $onceonly --pfff -d . -v -in $bam --out $outfn
   [ $? -ne 0 ] && exit 1
 done
