3e5e2d08d9b7e67f2a58fd7cdeec287c42ca342f
Pjotr Prins <pjotr.prins@thebird.nl>
2014-04-22 17:11:58 +0200

    mpileup

--- scripts/samtools_mpileup.sh
+++ scripts/samtools_mpileup.sh
@@ -6,13 +6,13 @@
 #
 # Example
 #
-#   ls -1 --color=never *_rmdup.bam | samtools_mpileup.sh
+#   pprins@wgs01:~/data/run5/bam_reduced/mpileup$ ls -1 --color=never ../*rmdup.bam |~/opt/somatic_pipeline/scripts/samtools_mpileup.sh
 
 samtools=$HOME/opt/bin/samtools
 onceonly=$HOME/izip/git/opensource/ruby/once-only/bin/once-only
 
 REFSEQ=/data/GENOMES/human_GATK_GRCh37/GRCh37_gatk.fasta
-BED="$HOME/kinome_design_SS_V2_110811_nochr_annot.bed"
+BED="$HOME/kinome_design_SS_V2_110811_nochr_annot_sorted.bed"
 phred=10  # 1:1000
 bed=$BED
 refgenome=$REFSEQ
@@ -25,8 +25,7 @@ echo "$samtools faidx $refgenome"|$onceonly --pfff -d . -v
 while read bam ; do
   echo ==== $samtools mpileup $bam...
   outfn=$(basename $bam .bam).mpileup
-  # check -E option
-  echo "$samtools mpileup -B -q $phred -f $refgenome -l $bed $bam > $bam.mpileup"|$onceonly --pfff -v -d . --out $outfn
+  # check -E option, you can also filter on $BED
+  echo "$samtools mpileup -B -C -E -S -q $phred -l $BED -f $refgenome $bam > $outfn"|$onceonly --pfff -v -d . --out $outfn
   [ $? -ne 0 ] && exit 1
-  exit 1
 done
