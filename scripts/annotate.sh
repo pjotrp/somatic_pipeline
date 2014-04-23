910e5ab489f38069889a5e8fc4d9273f0ad0fdfd
Pjotr Prins <pjotr.public01@thebird.nl>
2014-04-23 17:22:40 +0200

    annotate

---
+++ scripts/annotate.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# 
+# Simple annotator loop
+#
+#   ./annotator.sh < list
+#
+
+for x in *.snp ; do echo $x; ~/izip/git/opensource/ruby/once-only/bin/once-only /data/common_scripts/SAP42-testing/annotator.pl -in $x ; done
+
+annotate=/data/common_scripts/SAP42-testing/annotator.pl
+onceonly=$HOME/izip/git/opensource/ruby/once-only/bin/once-only
+
+while read infile ; do
+  echo ==== $annotate $infile...
+  echo "$annotate -in $infile"|$onceonly --pfff -d . -v
+  [ $? -ne 0 ] && exit 1
+done
