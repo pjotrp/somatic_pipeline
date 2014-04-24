c6dedc96cfefbf5e3a647ac359438066e63c1abb
Pjotr Prins <pjotr.prins@thebird.nl>
2014-04-24 17:31:42 +0200

    annotate

--- scripts/annotate.sh
+++ scripts/annotate.sh
@@ -10,6 +10,6 @@ onceonly=$HOME/izip/git/opensource/ruby/once-only/bin/once-only
 
 while read infile ; do
   echo ==== $annotate $infile...
-  echo "$annotate -in $infile"|$onceonly --pfff -d . -v
+  echo "$annotate -in $infile"|$onceonly -in $infile --out $infile.snv1 --out $infile.snv2 --pfff -d . -v
   [ $? -ne 0 ] && exit 1
 done
