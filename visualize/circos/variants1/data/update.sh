099703654f6e13bc037620246666c869ad452eca
Pjotr Prins <pjotr.public01@thebird.nl>
2014-05-17 11:39:25 +0200

    plot

--- visualize/circos/variants1/data/update.sh
+++ visualize/circos/variants1/data/update.sh
@@ -1,1 +1,2 @@
-grep -e 'BRCA\|ATM\|CHECK2\|TP53\|APC\|FAS\|RB1\|PLCG1' exome.txt > bc_suppressors.txt
+grep -P 'BRCA1|BRCA2|ATM$|CHEK2$|TP53$|APC$|FAS$|\tRB1$|PLCG1$' exome.txt > bc_suppressors.txt
+
