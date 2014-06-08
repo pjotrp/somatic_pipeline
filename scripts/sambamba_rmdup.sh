dad2ffd9cc058345a12bdc2fbda4ec290b7fe20b
pprins <pjotr.public01@thebird.nl>
2014-06-08 12:29:20 +0200

    HPC

--- scripts/sambamba_rmdup.sh
+++ scripts/sambamba_rmdup.sh
@@ -2,19 +2,20 @@
 # 
 # Simple rmdup loop
 #
-#   ./rmdup.sh < bamlist
+#   sambamba_rmdup.sh < bamlist
 #
 # Example
 #
 #   ls -1 --color=never *.bam | rmdup.sh
 
-sambamba=$HOME/opt/bin/sambamba
+sambamba=sambamba
 onceonly=$HOME/izip/git/opensource/ruby/once-only/bin/once-only
 
 while read bam ; do
   echo ==== $sambamba remove duplicates $bam...
   outfn=$(basename $bam .bam)_rmdup.bam
-  echo "$sambamba markdup -r $bam $outfn"| $onceonly --pfff -d . -v -in $bam --out $outfn
+  echo "$sambamba markdup -r $bam $outfn"| $onceonly --pfff --pbs '-q veryshort' -d . -v -in $bam --out $outfn
+  # echo "$sambamba markdup -r $bam $outfn"| $onceonly --pfff -d . -v -in $bam --out $outfn
   [ $? -ne 0 ] && exit 1
 done
 
