c0beebe6e88cc8649eb45c751cb84e8ec025adbd
pprins <pjotr.public01@thebird.nl>
2014-08-28 14:23:15 +0200

    Set to medium for large VCF files

--- scripts/annotate.sh
+++ scripts/annotate.sh
@@ -16,6 +16,6 @@ onceonly="once-only"
 while read infile ; do
   echo ==== $annotate $infile...
   # echo "$annotate -in $infile"|$onceonly -in $infile --out $infile.snv1 --out $infile.snv2 --pfff -d . -v
-  echo "$annotate -in $infile"|$onceonly -in $infile --out $infile.snv1 --out $infile.snv2 --pbs '-q veryshort' --pbs-type SGE --pfff -d . -v
+  echo "$annotate -in $infile"|$onceonly -in $infile --out $infile.snv1 --out $infile.snv2 --pbs '-q medium' --pbs-type SGE --pfff -d . -v
   [ $? -ne 0 ] && exit 1
 done
